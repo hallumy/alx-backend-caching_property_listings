9c9f9b274bd3ff1ce867c191a13793c049c2bbfa
Ahlam Abdulkhalek <alhamny@gmail.com>
2025-11-02 09:23:22 +0300

    Add signals.py file

--- properties/views.py
+++ properties/views.py
@@ -1,6 +1,5 @@
 from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
-from .models import Property
 from properties.utils import get_all_properties
 
 @cache_page(60 * 15)
