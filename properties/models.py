1769e608e02d7802070327ce95b3f903ab97d9b2
Ahlam Abdulkhalek <alhamny@gmail.com>
2025-11-02 08:31:06 +0300

    Add views.py file

--- properties/models.py
+++ properties/models.py
@@ -15,3 +15,6 @@ class Properties(models.Model):
     price       = models.DecimalField(verbose_name="Price", max_digits=10, decimal_places=2)
     location    = models.CharField(max_length=100)
     created_at  = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.cache import cache_page
+from .models import Properties
 
-# Create your views here.
+@cache_page(60 * 15)
+def property_list(request):
+    """
+    Returns a list of all property records.
+    Cached in Redis for 15 minutes to reduce database queries.
+    """
+    properties = Properties.objects.all()
+    data = list(properties)
+    return JsonResponse(data, safe=False)
+
