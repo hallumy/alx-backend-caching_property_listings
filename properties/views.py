f3761798d031f161dd7ed8df5de55dacabbf5e5b
Ahlam Abdulkhalek <alhamny@gmail.com>
2025-11-02 08:38:49 +0300

    Add views.py file

--- properties/views.py
+++ properties/views.py
@@ -8,7 +8,7 @@ def property_list(request):
     Returns a list of all property records.
     Cached in Redis for 15 minutes to reduce database queries.
     """
-    properties = Properties.objects.all()
+    properties = Properties.objects.all().values('id', 'title', 'location', 'price', 'description')
     data = list(properties)
     return JsonResponse(list(properties), safe=False)
 
